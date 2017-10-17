96222c3ce60705c9fc28c0746a03973bed0fdeda
Ahmad Hamed <ahmad.fhamed@gmail.com>
2017-10-17 16:33:30 +0300

    prevent form from jumping to the top of page when submitting

--- widget/app.js
+++ widget/app.js
@@ -1,4 +1,48 @@
-'use strict';
+function injectJotForm() {
+        var ifr = document.getElementById("JotFormIFrame");
+    if (window.location.href && window.location.href.indexOf("?") > -1) {
+        var get = window.location.href.substr(window.location.href.indexOf("?") + 1);
+        if (ifr && get.length > 0) {
+            var src = ifr.src;
+            src = src.indexOf("?") > -1 ? src + "&" + get : src + "?" + get;
+            ifr.src = src;
+        }
+    }
+    window.handleIFrameMessage = function (e) {
+        var args = e.data.split(":");
+
+        iframe = document.getElementById("JotFormIFrame");
+
+        if (!iframe) return;
+        switch (args[0]) {
+            case "scrollIntoView":
+                iframe.scrollIntoView();
+                break;
+            case "setHeight":
+                iframe.style.height = args[1] + "px";
+                break;
+            case "collapseErrorPage":
+                if (iframe.clientHeight > window.innerHeight) {
+                    iframe.style.height = window.innerHeight + "px";
+                }
+                break;
+            case "reloadPage":
+                window.location.reload();
+                break;
+        }
+        var isJotForm = (e.origin.indexOf("jotform") > -1) ? true : false;
+        if (isJotForm && "contentWindow" in iframe && "postMessage" in iframe.contentWindow) {
+            var urls = {"docurl": encodeURIComponent(document.URL), "referrer": encodeURIComponent(document.referrer)};
+            iframe.contentWindow.postMessage(JSON.stringify({"type": "urls", "value": urls}), "*");
+        }
+    };
+    if (window.addEventListener) {
+        window.addEventListener("message", handleIFrameMessage, false);
+    } else if (window.attachEvent) {
+        window.attachEvent("onmessage", handleIFrameMessage);
+    }
+}
+
 
 (function (angular) {
   angular.module('jotFormPluginWidget', ['ui.bootstrap'])
@@ -22,6 +66,7 @@
               var dummyData = {url: "https://form.jotform.me/60070086181448"};
               WidgetHome.data.content.url = dummyData.url;
             }
+              injectJotForm();
           };
           WidgetHome.error = function (err) {
             if (err && err.code !== STATUS_CODE.NOT_FOUND) {
